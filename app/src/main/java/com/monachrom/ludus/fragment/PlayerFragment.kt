e073fe9b242cc0ab318fa966fb7c85a44e374982
Kevin Jiang <kjiang1998@gmail.com>
2019-07-06 15:56:10 -0500

    Added Exoplayer library and Player fragment

---
+++ app/src/main/java/com/monachrom/ludus/fragment/PlayerFragment.kt
@@ -0,0 +1,34 @@
+package com.monachrom.ludus.fragment
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.monachrom.ludus.R
+import com.monachrom.ludus.viewmodel.PlayerViewModel
+
+class PlayerFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = PlayerFragment()
+    }
+
+    private lateinit var viewModel: PlayerViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_player, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(PlayerViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/monachrom/ludus/viewmodel/PlayerViewModel.kt
@@ -0,0 +1,7 @@
+package com.monachrom.ludus.viewmodel
+
+import androidx.lifecycle.ViewModel;
+
+class PlayerViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
